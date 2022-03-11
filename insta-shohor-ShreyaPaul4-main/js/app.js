b5626474b84a030fbd05f05043a0f5c96933f808
ShreyaPaul4 <shreyapaul505@gmail.com>
2022-03-11 23:11:45 +0600

    Description Bug Fixed

--- insta-shohor-ShreyaPaul4-main/js/app.js
+++ insta-shohor-ShreyaPaul4-main/js/app.js
@@ -27,7 +27,7 @@ const reportPost = (id) => {
 };
 
 const displayContent = (text) => {
-  return text.length < 30 ? 'text' : text.slice(0, 30) + "<span class='fw-bold'>... read more</span>";
+  return text.length < 30 ? 'text' : text.length === 27, text.slice(0, 30) + "<span class='fw-bold'>... read more</span>";
 };
 
 const switchTab = (id) => {
